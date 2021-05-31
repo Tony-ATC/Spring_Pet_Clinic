9155d746bf1e20f01879694850014819745e264a
Tony-ATC <anthony.calascibetta@student.intecbrussel.be>
2021-06-01 01:34:50 +0200

    Section 7 (PetType Map Service) - 7.122  - Added   - PetTypeMapService   - PetTypeService closes #23

---
+++ pet-clinic-data/src/main/java/udemy/springframework5guru/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package udemy.springframework5guru.springpetclinic.services;
+
+import udemy.springframework5guru.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
