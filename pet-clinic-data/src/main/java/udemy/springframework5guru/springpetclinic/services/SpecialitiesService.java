967431a2d7339667e4d10f02f356fabe453d817e
Tony-ATC <anthony.calascibetta@student.intecbrussel.be>
2021-06-01 02:34:01 +0200

    Section 7 (Specialities Map Impl) - 7.125  - Added   - SpecialitiesService   - SpecialityMapService * Added Specialities Map Impl closes #25

---
+++ pet-clinic-data/src/main/java/udemy/springframework5guru/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package udemy.springframework5guru.springpetclinic.services;
+
+import udemy.springframework5guru.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
