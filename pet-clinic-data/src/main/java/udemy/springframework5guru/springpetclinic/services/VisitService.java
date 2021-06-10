e35a246351bf3e7181bf12af9176e4ddd69aaa18
Tony-ATC <anthony.calascibetta@student.intecbrussel.be>
2021-06-10 20:26:49 +0200

    Section 8 (Visit Map Service) - 8.155  - Added   - VisitMapService   - VisitService * Implemented Map Impl for Visit Serice closes #40

---
+++ pet-clinic-data/src/main/java/udemy/springframework5guru/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package udemy.springframework5guru.springpetclinic.services;
+
+import udemy.springframework5guru.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
